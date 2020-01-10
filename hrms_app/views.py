7b0f2415650b59484e1743ef1ae6b00d80c1e614
Matias Perez <matias.perez@fluxit.com.ar>
2020-01-10 08:42:36 -0300

    Use template where show basic information about of user. Also send 404 error if person id does not exist

--- hrms_app/views.py
+++ hrms_app/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from .models import Person
+from django.template import loader
+from django.core import serializers
 
 # Create your views here.
 def index(request):
   return HttpResponse("Hello men!")
 
 def show(request, id):
-  p = Person.objects.get(id)
-  return HttpResponse("person")
+  template = loader.get_template("person/index.html")
+  try:
+    p = Person.objects.get(id=id)
+  except Person.DoesNotExist:
+    raise Http404("Person does not exist")
+  return HttpResponse(template.render({
+    'lastName': p.lastName,
+    'firstName': p.firstName
+  }, request))
+
+  
